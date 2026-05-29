8e38e2389b3c0b606cf01eeaa598b346faea39e5
maneeshkumarawasthi <maneesh15981@gmail.com>
2026-05-29 19:16:20 +0530

    llm changes

--- src/chatmodel_hf_api.py
+++ src/chatmodel_hf_api.py
@@ -5,14 +5,25 @@ load_dotenv()
 # from config import set_environment
 # set_environment()
 
+#llm = HuggingFaceEndpoint(
+#    repo_id="HuggingFaceH4/zephyr-7b-beta",
+#    task="text-generation",
+#    max_new_tokens=20
+#)
+
+#model = ChatHuggingFace(llm=llm)
+
+#result = model.invoke("What is the capital of India?")
+
+#print(result.content)
 llm = HuggingFaceEndpoint(
-    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
     task="text-generation",
     max_new_tokens=20
 )
 
-model = ChatHuggingFace(llm=llm)
 
-result = model.invoke("What is the capital of India?")
-
-print(result.content)
+if __name__ == "__main__":
+    model = ChatHuggingFace(llm=llm)
+    result = model.invoke("What is the capital of India?")
+    print(result.content)
